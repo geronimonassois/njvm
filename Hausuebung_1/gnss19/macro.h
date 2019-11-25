c9df9d973c5bc5f63c2f1e4670d1900abe3277c1
Geronimo Nassois <geronimo.nassois@mni.thm.de>
2019-11-25 18:03:19 +0100

    cleanup commit

---
+++ Hausuebung_1/gnss19/macro.h
@@ -0,0 +1,11 @@
+/* file contain macros (duh)
+ * @EQSTRING tests strings on equality returns bool value
+ * @IMMEDIATE bitcuts opcode, returns immediate value
+ * @OPCODE bitcuts immediate, returns opcode value
+ */
+
+#define EQSTRING(param, testString) !strcmp(param, testString)
+#define IMMEDIATE(x) ((x) & 0x00FFFFFF)
+#define OPCODE(x) (((unsigned int) x) >> 24)
+#define SIGN_EXTEND(i) ((i) & 0x00800000 ? (i) | 0xFF000000 : (i))
+
