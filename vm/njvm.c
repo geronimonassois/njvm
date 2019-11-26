d9cfdff9b74438f7ac790c6d99b14931b0896c4d
Daniel Wagner <wagner.daniel@mni.thm.de>
2019-11-26 11:55:14 +0100

    exception handling

--- vm/njvm.c
+++ vm/njvm.c
@@ -13,7 +13,7 @@
 
 int stack[STACK_SIZE];
 int* static_variables;
-int return_stack[RETURN_STACK_SIZE];
+int return_value;
 
 int no_of_static_variables;
 int no_of_instructions;
@@ -200,7 +200,6 @@ void exception(char* message, const char *func, int line){
     exit(1);
 }
 
-
 int pop_Stack(){
     if(stack_pointer <= 0){
         exception("Stackunderflow Exception", __func__, __LINE__);
@@ -215,18 +214,36 @@ void push_Stack(int immediate){
     stack[stack_pointer++] = immediate;
 }
 
-int pop_return_Stack(){
-    if(return_stack_pointer <= 0){
-        exception("Return-Stackunderflow Exception", __func__, __LINE__);
+void pop_Global(int immediate){
+    if(immediate <= 0){
+        exception("Global Stackunderflow Exception", __func__, __LINE__);
     }
-    return return_stack[--return_stack_pointer];
+    int var = pop_Stack();
+    static_variables[immediate] = var;
 }
 
-void push_return_Stack(int immediate){
-    if(return_stack_pointer == RETURN_STACK_SIZE-1){
-        exception("Return-Stackoverflow Exception", __func__, __LINE__);
+void push_Global(int immediate){
+    if(immediate < no_of_static_variables){
+        exception("Global Stackoverflow Exception bei Funktion:", __func__, __LINE__);
     }
-    return_stack[return_stack_pointer++] = immediate;
+    int var = static_variables[immediate];
+    push_Stack(var);
+}
+
+void pop_local(int memory_Adress){
+    if(memory_Adress <= 0){
+        exception("Stackunderflow Exception", __func__, __LINE__);
+    }
+    int var = pop_Stack();
+    stack[memory_Adress] = var;
+}
+
+void push_local(int memory_Adress){
+    if(memory_Adress == STACK_SIZE-1){
+        exception("Stackoverflow Exception bei Funktion:", __func__, __LINE__);
+    }
+    int val = stack[memory_Adress];
+    push_Stack(val);
 }
 
 int halt (int immediate){
@@ -276,6 +293,9 @@ int divi (int immediate){
 int mod (int immediate){
     int modulent = pop_Stack();
     int numerator = pop_Stack();
+    if(modulent == 0){
+        exception("Modulo by Zero Exception", __func__, __LINE__);
+    }
     pushc(numerator % modulent);
     program_counter++;
     return 0;
@@ -310,38 +330,34 @@ int wrchr (int immediate){
     return 0;
 }
 
-/*
- * TO-DO: immediates pruefen
- * */
 int pushg(int immediate){
-    int var = static_variables[immediate];
-    push_Stack(var);
+    push_Global(immediate);
     program_counter++;
     return 0;
 }
 
 int popg(int immediate){
-    int var = pop_Stack();
-    static_variables[immediate] = var;
+    pop_Global(immediate);
     program_counter++;
     return 0;
 }
 
 int pushl(int immediate) {
-    int val = stack[frame_pointer+immediate];
-    push_Stack(val);
+    push_local(frame_pointer+immediate);
     program_counter++;
     return 0;
 }
 
 int popl(int immediate){
-    int var = pop_Stack();
-    stack[frame_pointer+immediate] = var;
+    pop_local(frame_pointer+immediate);
     program_counter++;
     return 0;
 }
 
 int asf(int immediate){
+    if((stack_pointer + immediate) > STACK_SIZE-1){
+        exception("Assamble Stackframe Stackoverflow Exception bei Funktion:", __func__, __LINE__);
+    }
     push_Stack(frame_pointer);
     frame_pointer = stack_pointer;
     stack_pointer += immediate;
@@ -483,14 +499,13 @@ int drop(int immediate){
 }
 
 int pushr(int immediate){
-    int return_val = pop_return_Stack();
-    push_Stack(return_val);
+    push_Stack(return_value);
     program_counter++;
     return 0;
 }
 
 int popr(int immediate){
-    push_return_Stack(pop_Stack());
+    return_value = pop_Stack();
     program_counter++;
     return 0;
 }
